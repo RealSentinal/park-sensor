46275aa72f4e139a14ad6d376ae707b3c79f7583
Ahmet Burak Arslan <69633694+RealSentinal@users.noreply.github.com>
2024-08-24 17:10:39 +0200

    Park sensor

---
+++ sketch.ino
@@ -0,0 +1,50 @@
+#define trig_pin 16
+#define echo_pin 17
+#define buzzer_pin 4
+#define led_pin 15
+
+void setup() {
+  Serial1.begin(9600);
+  pinMode(trig_pin, OUTPUT);
+  pinMode(echo_pin, INPUT); 
+  pinMode(led_pin, OUTPUT);
+  pinMode(buzzer_pin, OUTPUT);
+}
+void loop() {
+  long distance, duration;
+  digitalWrite(trig_pin, LOW);
+  delayMicroseconds(2);
+  digitalWrite(trig_pin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trig_pin, LOW);
+  duration = pulseIn(echo_pin, HIGH);
+  distance = (duration / 2) / 29.1;
+  if(distance <= 100) {
+    digitalWrite(buzzer_pin, HIGH);
+    digitalWrite(led_pin, HIGH);
+    delay(100);
+    digitalWrite(buzzer_pin, LOW);
+    digitalWrite(led_pin, LOW);
+    delay(100);
+  } 
+  else if(distance <= 200) {
+    digitalWrite(buzzer_pin, HIGH);
+    digitalWrite(led_pin, HIGH);
+    delay(500);
+    digitalWrite(buzzer_pin, LOW);
+    digitalWrite(led_pin, LOW);
+    delay(500);
+  } 
+  else if(distance <= 300) {
+    digitalWrite(buzzer_pin, HIGH);
+    digitalWrite(led_pin, HIGH);
+    delay(1000);
+    digitalWrite(buzzer_pin, LOW);
+    digitalWrite(led_pin, LOW);
+    delay(1000);
+  } 
+  else {
+    digitalWrite(buzzer_pin, LOW);
+    digitalWrite(led_pin, LOW);
+  }
+}
